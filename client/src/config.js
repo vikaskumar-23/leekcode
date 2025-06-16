2f745a7f22759cc9bd8da3789f2f37137c450013
vikaskumar-23 <devilduck20@gmail.com>
2025-06-17 00:17:13 +0530

    Update build configuration to handle warnings

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,7 @@
+import axios from 'axios';
+
 // API configuration
 export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
 // Configure axios defaults
-import axios from 'axios';
 axios.defaults.withCredentials = true; 
